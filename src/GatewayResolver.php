ea79b141ba0924fc01fee9df980a3b63b4dd07d9
mekazemi8 <mekazemi8@gmail.com>
2023-08-02 13:51:40 +0330

    Add BazarPay Gateway

--- src/GatewayResolver.php
+++ src/GatewayResolver.php
@@ -22,6 +22,7 @@ use Larabookir\Gateway\Exceptions\PortNotFoundException;
 use Larabookir\Gateway\Exceptions\InvalidRequestException;
 use Larabookir\Gateway\Exceptions\NotFoundTransactionException;
 use Illuminate\Support\Facades\DB;
+use Larabookir\Gateway\BazarPay\BazarPay;
 
 class GatewayResolver
 {
@@ -36,7 +37,7 @@ class GatewayResolver
 	/**
 	 * Keep current port driver
 	 *
-	 * @var Mellat|Saman|Sadad|Zarinpal|Payline|JahanPay|Parsian|Pay|Saderat|Saderatnew|Idpay|Alfacoins|Payping|Plisio
+	 * @var Mellat|Saman|Sadad|Zarinpal|Payline|JahanPay|Parsian|Pay|Saderat|Saderatnew|Idpay|Alfacoins|Payping|Plisio|Bazarpay
 	 */
 	protected $port;
 
@@ -63,7 +64,7 @@ class GatewayResolver
 	 */
 	public function getSupportedPorts()
 	{
-		return [Enum::MELLAT, Enum::SADAD, Enum::ZARINPAL, Enum::PAYLINE, Enum::JAHANPAY, Enum::PARSIAN, Enum::PASARGAD, Enum::SAMAN, Enum::PAY, Enum::SADERAT, Enum::SADERATNEW, Enum::IDPAY, Enum::ALFACOINS, Enum::PAYPING, Enum::PLISIO];
+		return [Enum::MELLAT, Enum::SADAD, Enum::ZARINPAL, Enum::PAYLINE, Enum::JAHANPAY, Enum::PARSIAN, Enum::PASARGAD, Enum::SAMAN, Enum::PAY, Enum::SADERAT, Enum::SADERATNEW, Enum::IDPAY, Enum::ALFACOINS, Enum::PAYPING, Enum::PLISIO, Enum::BAZARPAY];
 	}
 
 	/**
@@ -164,8 +165,10 @@ class GatewayResolver
 		} elseif ($port InstanceOf Payping) {
 			$name = Enum::PAYPING;
 		} elseif ($port InstanceOf Plisio) {
-			$name = Enum::PLISIO;
-		} elseif(in_array(strtoupper($port),$this->getSupportedPorts())){
+            $name = Enum::PLISIO;
+        } elseif ($port InstanceOf BazarPay) {
+            $name = Enum::BAZARPAY;
+        } elseif(in_array(strtoupper($port),$this->getSupportedPorts())){
 			$port=ucfirst(strtolower($port));
 			$name=strtoupper($port);
 			$class=__NAMESPACE__.'\\'.$port.'\\'.$port;
